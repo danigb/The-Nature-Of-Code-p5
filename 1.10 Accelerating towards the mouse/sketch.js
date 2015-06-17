cb9163dec1a57a69d1b9cacfeb542f715aacfc90
danigb <danigb@gmail.com>
2015-06-17 20:38:59 +0200

    Chapter 1 completed

---
+++ 1.10 Accelerating towards the mouse/sketch.js
@@ -0,0 +1,51 @@
+var radius = 64
+
+function Mover (p) {
+  this.location = p.createVector(p.random(p.width), p.random(p.height))
+  this.velocity = p.createVector(p.random(-2, 2), p.random(-2, 2))
+
+  this.update = function () {
+    var mouse = p.createVector(p.mouseX, p.mouseY)
+    var acceleration = p5.Vector.sub(mouse, this.location).normalize().mult(0.5)
+    this.velocity.add(acceleration)
+    this.velocity.limit(5)
+    this.location.add(this.velocity)
+  }
+
+  this.display = function () {
+    p.stroke(0)
+    p.fill(175)
+    p.ellipse(this.location.x, this.location.y, radius, radius)
+  }
+
+  this.checkEdges = function () {
+    this.location.x = inBounds(this.location.x, 0, p.width)
+    this.location.y = inBounds(this.location.y, 0, p.height)
+  }
+
+  function inBounds (value, min, max) {
+    if (value < min) {
+      return max
+    } else if (value > max) {
+      return min
+    } else {
+      return value
+    }
+  }
+}
+
+new p5(function (p) {
+  var mover
+
+  p.setup = function () {
+    p.createCanvas(640, 360)
+    mover = new Mover(p)
+  }
+
+  p.draw = function () {
+    p.background(250)
+    mover.update()
+    mover.checkEdges()
+    mover.display()
+  }
+})
--- README.md
+++ README.md
@@ -2,22 +2,22 @@
 
 This is my own version of [The Nature Of Code](http://natureofcode.com/) examples in [p5.js](http://p5js.org/)
 
-__This is a personal exercise and a work in progress.__
+__This is a personal exercise and a work in progress.__ For a reference implementation: https://github.com/shiffman/The-Nature-of-Code-Examples-p5.js
 
-For a reference implementation: https://github.com/shiffman/The-Nature-of-Code-Examples-p5.js
-
-There are some differences from the reference implementation:
+There are some differences between the reference implementation and this one:
 
 - Most important, I use the p5 instantiation mode (instead of global mode)
 - I want the code [standard](https://github.com/feross/standard) compilant
+- The code style is what I use right now, not necessary for newbies
 
 
 ## How to?
 
-Clone the repo. Install a server (with node or iojs: `npm i -g http-server`) and start it:
+Clone the repo and start a http server at root. With node/iojs:
 
 ```bash
-http-server
+npm install -g http-server  # installs the server
+http-server                 # starts the server
 ```
 
 ## License
